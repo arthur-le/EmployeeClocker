0abcb12d8ad79c7737139de9903a205ecd248910
Arthur Le <arthurle9@gmail.com>
2016-01-29 10:44:16 -0800

    Added toggle button to track user as he moves. Added coordinates to usertextfield

--- EmployeeClocker/UserMapViewController.swift
+++ EmployeeClocker/UserMapViewController.swift
@@ -25,11 +25,21 @@ class UserMapViewController: UITableViewController,MKMapViewDelegate, CLLocation
     @IBOutlet weak var eventTypeSegmentedControl: UISegmentedControl!
     @IBOutlet weak var radiusTextField: UITextField!
     @IBOutlet var usernameTextField: UITextField!
+
+    
+    @IBOutlet var mySwitch: UISwitch!
+    
+    
     
     @IBOutlet var userText: UILabel!
     @IBOutlet var employeeUsername: UILabel!
+    @IBOutlet var detailTextField: UITextField!
     
-    let manager = CLLocationManager()
+    
+       
+    
+    
+    var manager:CLLocationManager!
 
 
     @IBAction func zoomToUserButton(sender: UIButton) {
@@ -42,6 +52,9 @@ class UserMapViewController: UITableViewController,MKMapViewDelegate, CLLocation
     
     var delegate: AddGeotificationsViewControllerDelegate!
     
+    //will hold array of locations
+    var myLocations: [CLLocation] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         employeeUsername.text = EmployeeLoginViewController().getUsername()
@@ -53,15 +66,64 @@ class UserMapViewController: UITableViewController,MKMapViewDelegate, CLLocation
         
       
         
-        let manager = CLLocationManager()
-      
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        
         if CLLocationManager.authorizationStatus() == .NotDetermined {
             manager.requestWhenInUseAuthorization()
         }
+        
+        //check for location changes
+        //if locations change, calls func locationManager
         if CLLocationManager.locationServicesEnabled() {
             manager.startUpdatingLocation()
         }
+        
+        
+        //Setup our Map View
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        
      
+    }
+    
+    //sets coordinates to details textfield everytime user moves
+    func locationManager(manager:CLLocationManager, didUpdateLocations locations:[CLLocation]) {
+        print("LOCATION UPDATINGGGG")
+        
+        
+        if mySwitch.on {
+            print("Switch is on")
+            
+            // switch is on
+            let spanX = 0.007
+            let spanY = 0.007
+            var newRegion = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: MKCoordinateSpanMake(spanX, spanY))
+            mapView.setRegion(newRegion, animated: true)
+            
+        } else {
+            print("Switch is off")
+            
+        }
+        
+        
+        
+        detailTextField.text = "\(locations[0])"
+        
+        //creates location points into myLocations
+        myLocations.append(locations[0] as! CLLocation)
+        
+        if (myLocations.count > 1){
+            var sourceIndex = myLocations.count - 1
+            var destinationIndex = myLocations.count - 2
+            
+            let c1 = myLocations[sourceIndex].coordinate
+            let c2 = myLocations[destinationIndex].coordinate
+            var a = [c1, c2]
+            var polyline = MKPolyline(coordinates: &a, count: a.count)
+            mapView.addOverlay(polyline)
+        }
     }
     
     @IBAction func textFieldEditingChanged(sender: UITextField) {
