300ecfccb470def466ca601ca76d729857b07899
Arthur Le <arthurle9@gmail.com>
2016-02-05 11:48:20 -0800

    Added working queries. Query calls based off entered username

--- EmployeeClocker/ClockInClockOutViewController.swift
+++ EmployeeClocker/ClockInClockOutViewController.swift
@@ -26,18 +26,56 @@ class ClockInClockOutViewController: UIViewController, CLLocationManagerDelegate
 
     }
 
-    @IBAction func clockInAction(sender: UIButton){
-        //let query = PFQuery(className: "UserLocation")
-        //query.whereKey("username", equalTo: "arthurle")
-        //query.selectKeys(["username", "clockInTime", "clockOutTime","location"])
-        //let arrayCurrentData = try! query.findObjects() as [PFObject]
+    @IBAction func clockInAction(sender: UIButton)
+    {
+        let query = PFQuery(className: "UserLocations")
+        //query.findObjectsInBackgroundWithBlock{(objects,error) -> Void in
+          //  if error == nil
+            //{
+              //  if let returnedobjects = objects
+                //{
+                  //  for object in returnedobjects
+                    //{
+                      //  print("Username is: ", object["username"] as! String)
+                        //print("Clock in date is: ", object["clockInTime"] as! NSDate)
+                        //print("Location is: ", object["location"] as! String)
+                    //}
+                //}
+            //}
+            
+        //}
         
-       //print(arrayCurrentData)
-
-
-   
+        //query constraint works cool
+        query.whereKey("username", equalTo: usernameLabel.text!)
+        //line will test all above constraints
+        query.findObjectsInBackgroundWithBlock{(objects,error) -> Void in
+            if error == nil{
+                let arrayCurrentData = try! query.findObjects() as [PFObject]
+                
+                print("Testing constraints: ", arrayCurrentData)
+            }
+        }
         
     }
+    
+    
+    @IBAction func clockOutAction(sender: UIButton) {
+        
+        let query = PFQuery(className: "UserLocations")
+        //query constraint works cool
+        query.whereKey("username", equalTo:usernameLabel.text!)
+        //line will test all above constraints
+        query.findObjectsInBackgroundWithBlock{(objects,error) -> Void in
+            if error == nil{
+                let arrayCurrentData = try! query.findObjects() as [PFObject]
+                
+                print("Testing constraints: ", arrayCurrentData)
+            }
+        }
+    }
+    
+    
+    
     
     @IBAction func confirmLocationButton(sender: UIButton) {
         //Setup our Location Manager
